ca04b5f8bd7d5d991769ea9c13abbec765e90694
Rico Schrage <rico.schrage@gmail.com>
2016-03-22 19:28:04 +0100

    Fixed some sonar issues.

--- ContentAdapter.java
+++ ContentAdapter.java
@@ -9,46 +9,46 @@ public class ContentAdapter implements Content {
 
     @Override
     public void onBind(ScreenContext screenContext) {
-
+        //default: do nothing
     }
 
     @Override
     public void onActivate(ScreenId predecessor) {
-
+        //default: do nothing
     }
 
     @Override
     public void onDeactivate(ScreenId successor) {
-
+        //default: do nothing
     }
 
     @Override
     public void onUpdate() {
-
+        //default: do nothing
     }
 
     @Override
     public void onRender() {
-
+        //default: do nothing
     }
 
     @Override
     public void onPause() {
-
+        //default: do nothing
     }
 
     @Override
     public void onResume() {
-
+        //default: do nothing
     }
 
     @Override
     public void onResize(int width, int height) {
-
+        //default: do nothing
     }
 
     @Override
     public void dispose() {
-
+        //default: do nothing
     }
 }
