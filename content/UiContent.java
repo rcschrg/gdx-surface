442bf23d379602143ed5a7764a8bc776ad28f8ef
Rico Schrage <rico.schrage@gmail.com>
2016-04-04 01:20:14 +0200

    Some preparations for the hud.

---
+++ content/UiContent.java
@@ -0,0 +1,92 @@
+package de.verygame.square.core.content;
+
+import com.badlogic.gdx.Gdx;
+
+import de.verygame.square.core.Content;
+import de.verygame.square.core.ScreenContext;
+import de.verygame.square.core.ScreenId;
+import de.verygame.square.core.event.EventEmitter;
+import de.verygame.square.resource.Resource;
+import de.verygame.square.resource.ResourceHandler;
+import de.verygame.square.util.glmenu.impl.GLMenuStage;
+
+/**
+ * @author Rico Schrage
+ */
+public abstract class UiContent extends EventEmitter implements Content {
+
+    /** Resource handler which contains all ui resources */
+    protected final ResourceHandler res;
+
+    /** Stage, which loads the xml */
+    protected GLMenuStage stage;
+
+    /** Context of the screen */
+    protected ScreenContext context;
+
+    /**
+     * Creates the user interface of the game.
+     *
+     *  @param res handler, which contains an applicable skin, the markup file and the language pack.
+     */
+    public UiContent(ResourceHandler res) {
+        this.res = res;
+    }
+
+    /**
+     * Should return the xml resource, which describes the xml of the glmenuStage.
+     *
+     * @return UiResource of the type XML
+     */
+    protected abstract Resource defineXML();
+
+    @Override
+    public void onBind(ScreenContext context) {
+        this.context = context;
+
+        this.stage = new GLMenuStage(context.getBatch(), res.getXMLAsStream(defineXML()), res);
+        this.stage.setViewport(context.getViewport());
+        this.stage.bind(this);
+        this.stage.loadMenu();
+    }
+
+    @Override
+    public void onActivate(ScreenId predecessor) {
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void onUpdate() {
+        stage.act();
+    }
+
+    @Override
+    public void onRender() {
+        stage.draw();
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+
+    @Override
+    public void onDeactivate(ScreenId successor) {
+        //default: do nothing
+    }
+
+    @Override
+    public void onResize(int width, int height) {
+        //default: do nothing
+    }
+
+    @Override
+    public void onPause() {
+        //default: do nothing
+    }
+
+    @Override
+    public void onResume() {
+        //default: do nothing
+    }
+}
