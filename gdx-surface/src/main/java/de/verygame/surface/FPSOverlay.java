55a74afe9bb9a6b742bf828c7d6501e1cbfae670
Rico Schrage <rico.schrage@gmail.com>
2017-05-14 20:10:28 +0200

    Fixed some bugs.

--- gdx-surface/src/main/java/de/verygame/surface/FPSOverlay.java
+++ gdx-surface/src/main/java/de/verygame/surface/FPSOverlay.java
@@ -11,17 +11,36 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class FPSOverlay {
     private static final Color color = new Color(1f, 1f, 1f, 0.5f);
+    private static final float TTD = 3f;
 
     private BitmapFont bitmapFont;
+    private CharSequence display;
+    private float remainingTTD = TTD;
+    private float x;
+    private float y;
 
     public FPSOverlay() {
         bitmapFont = new BitmapFont();
         bitmapFont.setColor(color);
         bitmapFont.getData().setScale(Gdx.graphics.getHeight()/600f, Gdx.graphics.getHeight()/600f);
+        x = (5f*Gdx.graphics.getDensity());
+        y = (3f*Gdx.graphics.getDensity()) + 15*bitmapFont.getScaleX();
+
+        update();
+    }
+
+    public void update() {
+        display = String.valueOf(Gdx.graphics.getFramesPerSecond());
     }
 
     public void render(Batch batch) {
-        bitmapFont.draw(batch, String.valueOf(Gdx.graphics.getFramesPerSecond()),  (int) (5f*Gdx.graphics.getDensity()), (int) (3f*Gdx.graphics.getDensity()) + 15*bitmapFont.getScaleX());
+        remainingTTD -= Gdx.graphics.getDeltaTime();
+        if (remainingTTD <= 0) {
+            update();
+            remainingTTD = TTD;
+        }
+
+        bitmapFont.draw(batch, display, x, y);
     }
 
 }
